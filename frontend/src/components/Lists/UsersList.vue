2623fc1ba17e20450c4d22ae6439fd8140344418
ghospadi <Lolr2223@gmail.com>
2023-02-22 16:26:23 +0200

    Changes for build

--- frontend/src/components/Lists/UsersList.vue
+++ frontend/src/components/Lists/UsersList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card v-for="user in users" :key="user.id" class="rounded-lg mb-2 pa-4" :class="{'users': display !== 'xs', 'phone-users': display === 'xs', 'blocked': user.banned, 'border': !user.banned}">
-    <v-col cols="12" class="d-flex justify-space-between align-center pa-4">
+    <v-col cols="12" class="d-flex justify-space-between align-center pa-2">
       <h4 class="text-center">{{ user.id }}</h4>
       <h5 class="text-center">{{ user.name }} ({{user.email}}) </h5>
       <p class="text-center">{{ user.country }}</p>
